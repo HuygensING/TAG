1eb9b132212c075c69481d806e36f064f0046517
Elli Bleeker <elli.bleeker@gmail.com>
2017-03-02 17:18:46 +0100

    Factory method n for NodeMatcher; builder pattern for children

--- src/test/java/prioritised_xml_collation/NodeMatcher.java
+++ src/test/java/prioritised_xml_collation/NodeMatcher.java
@@ -6,6 +6,7 @@ import org.hamcrest.Matcher;
 import org.hamcrest.collection.IsIterableContainingInOrder;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -20,6 +21,10 @@ import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 public class NodeMatcher extends BaseMatcher<Node> {
     private SegmentMatcher segmentMatcher;
     private List<NodeMatcher> childrenMatcher;
+
+    private NodeMatcher(SegmentMatcher segmentMatcher) {
+        this.segmentMatcher = segmentMatcher;
+    }
 
     @Override
     // check whether given object is Node object:
@@ -73,4 +78,20 @@ public class NodeMatcher extends BaseMatcher<Node> {
         else {description.appendText("but actual object is "+object.getClass().getName());
         }
     }
+
+    // Factory method: each node has a segment
+    public static NodeMatcher n(SegmentMatcher segmentMatcher) {
+        return new NodeMatcher(segmentMatcher);
+    }
+
+    // Builder pattern: a Segment Node has one or more child nodes (list)
+    // Child nodes could be empty (leaf node) or have one or more child nodes (list) again
+    // "One or more" is indicated with '...'
+    public NodeMatcher childrenMatcher(NodeMatcher... childrenMatcher) {
+        this.childrenMatcher = Arrays.asList(childrenMatcher);
+        return this;
+    }
+
+
+
 }

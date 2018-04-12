43b24fcd193675c1eeca1dddef28c3bcfbc0a42e
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2018-04-12 12:39:02 +0200

    replace 'while' with 'foreach'

--- TAG-diff/src/main/java/prioritised_xml_collation/AlignedNonAlignedSegmenter.java
+++ TAG-diff/src/main/java/prioritised_xml_collation/AlignedNonAlignedSegmenter.java
@@ -19,8 +19,8 @@ package prioritised_xml_collation;
  * limitations under the License.
  * #L%
  */
+
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class AlignedNonAlignedSegmenter implements SegmenterInterface {
@@ -30,20 +30,19 @@ public class AlignedNonAlignedSegmenter implements SegmenterInterface {
         Cell lastCell = editTable.iterator().next();
         Cell currentCell = null;
         // CellIterator iterates cells:
-        Iterator<Cell> iterateTable = editTable.iterator();
-        // As long as the pointer can move up in the editTable
-        while (iterateTable.hasNext()) {
-            // move one cell up
-            currentCell = iterateTable.next();
-            // stateChange if the type of the lastCell is not the same as the currentCell
-            Boolean stateChange = lastCell.match != currentCell.match;
-            if (stateChange) {
-                // insert the segment to the superwitness list at the first position (position "0")
-                superwitness.add(0, editTable.createSegmentOfCells(currentCell, lastCell));
-                // change the pointer
-                lastCell = currentCell;
-            }
+      // As long as the pointer can move up in the editTable
+      for (final Cell anEditTable : editTable) {
+        // move one cell up
+        currentCell = anEditTable;
+        // stateChange if the type of the lastCell is not the same as the currentCell
+        Boolean stateChange = lastCell.match != currentCell.match;
+        if (stateChange) {
+          // insert the segment to the superwitness list at the first position (position "0")
+          superwitness.add(0, editTable.createSegmentOfCells(currentCell, lastCell));
+          // change the pointer
+          lastCell = currentCell;
         }
+      }
         // process the final cell in de EditGraphTable (additions/omissions at the beginning of the witnesses)
         if (lastCell!=currentCell) {
             // insert the segment to the superwitness list at the first position (position "0")

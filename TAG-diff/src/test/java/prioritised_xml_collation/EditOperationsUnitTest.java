c3222acbdbca6d4b7c234b13cc71874c84795c13
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2018-04-20 14:29:31 +0200

    add copyright headers

--- TAG-diff/src/test/java/prioritised_xml_collation/EditOperationsUnitTest.java
+++ TAG-diff/src/test/java/prioritised_xml_collation/EditOperationsUnitTest.java
@@ -1,5 +1,25 @@
 package prioritised_xml_collation;
 
+/*-
+ * #%L
+ * TAG-diff
+ * =======
+ * Copyright (C) 2016 - 2018 HuC DI (KNAW)
+ * =======
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import org.junit.Test;
 
 import javax.xml.stream.XMLStreamException;
@@ -9,12 +29,7 @@ import java.util.List;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 import static org.junit.Assert.assertThat;
-import static prioritised_xml_collation.Segment.Type.aligned;
-import static prioritised_xml_collation.Segment.Type.replacement;
-import static prioritised_xml_collation.SegmentMatcher.sM;
-import static prioritised_xml_collation.TAGTokenContentMatcher.t;
 
 /*
  * Created by Ronald Haentjens Dekker
